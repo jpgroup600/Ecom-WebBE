1c3a4ca43e8bdf76a029b5562cf075694b33f946
jpgroup600 <133649099+jpgroup600@users.noreply.github.com>
2024-11-30 01:38:44 +0900

    testing

--- Models/User.js
+++ Models/User.js
@@ -1,44 +1,62 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-},
-email: {
-    type: String,
-    required: true,
-},
-password: {
-    type: String,
-    required: true,
-},
-phoneNumber: {
-    type: String,
-    required: true,
-},
-birthDate: {
-    type: String,
-    required: false,
-},
-address: {
-    type: String,
- 
-},
-signupPath: {
-    type: String,
-},
-influenceType: {
-    type: [String],
-    required: true,
-},
-
-gender: {
-    type: String,
-    required: true,
-},
-
-
+    name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      businessName: {
+        type: String,
+      },
+      address: {
+        type: String,
+        required: false,
+      },
+      PhoneNumber: {
+        type: String,
+        required: true,
+      },
+      signupPath: {
+        type: String,
+        required: false,
+      },
+      TextFild1: {
+        type: String,
+        required: false,
+      },
+      TextFild2: {
+        type: String,
+        required: false,
+      },
+      TextFild3: {
+        type: String,
+        required: false,
+      },
+      image1: {
+        type: String,
+        required: false,
+      },
+      image2: {
+        type: String,
+        required: false,
+      },
+      image3: {
+        type: String,
+        required: false,
+      },
+      birthdate: {
+        type: Date,
+        required: false,
+      },
 
 });
 const UserModel = mongoose.model('users', UserSchema);
